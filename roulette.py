ada9665e3296c78839b105a640a94d3e34b9ff93
LePhroger <132900761+LePhroger@users.noreply.github.com>
2025-01-24 14:14:07 +0000

    made basic roulette

--- roulette.py
+++ roulette.py
@@ -1,3 +1,27 @@
+import random as rd
+
 RED = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
 BLACK = [2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35]
 
+Player_input = 0
+Win_number = 0
+Win_colour = ""
+
+Active = True
+
+while Active:
+    Player_input = int(input("What is your bet (1-36)?"))
+
+    Win_number = rd.randint(1,36)
+
+    if Win_number in RED:
+        Win_colour = "RED"
+    elif Win_number in BLACK:
+        Win_colour = "BLACK"
+
+    print("The winning number is",Win_number,Win_colour)
+
+    if Win_number == Player_input:
+        print('Winner')
+    else:
+        print('Loss')        
